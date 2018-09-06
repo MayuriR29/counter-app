d758914ff55d84d500a1f3d59003c42bd0c41195
MayuriR29 <mayuri.gr@gmail.com>
2018-09-06 10:29:48 +0800

    Added onClick handler

--- src/Component/counter.js
+++ src/Component/counter.js
@@ -9,12 +9,16 @@ class Counter extends Component {
     const count = this.state.count;
     return count === 0 ? 'Zero':count;
   }
-
+  handleIncrement=()=>{
+    this.setState({
+      count:this.state.count+1
+    })
+  }
   render() {
     return (
       <div>
         <button className={this.getBadgeClass()} >{this.formatCount()}</button>
-        <button className="btn btn-secondary btn-sm" >Increment</button>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm" >Increment</button>
         <ul>{this.state.tags.map((eachtag,index)=>{
           return <li key={index}>{eachtag}</li>
         })}</ul>
