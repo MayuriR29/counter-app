b999b9a9a6c5f093e31ca9a841e3f761fc0c772a
MayuriR29 <mayuri.gr@gmail.com>
2018-09-07 14:23:06 +0800

    Changed to functional component

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,21 +1,18 @@
-import React, { Component } from "react";
+import React  from "react";
 
-class NavBar extends Component {
-  render() {
-    console.log("hi", this.props.totalCounters.length);
-    return (
-      <div>
-        <nav className="navbar navbar-light bg-light">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters.length}
-          </span>
-        </nav>
-      </div>
-    );
-  }
-}
+const NavBar = (props) => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters.length}
+        </span>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
