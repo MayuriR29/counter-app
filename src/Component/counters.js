b21a6d27afeee4070369126c8ad48a0ce560338d
MayuriR29 <mayuri.gr@gmail.com>
2018-09-07 15:17:57 +0800

    refactored to functional component

--- src/Component/counters.js
+++ src/Component/counters.js
@@ -1,25 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import Counter from "./Counter";
 
-class Counters extends Component {
-  render() {
-    const { onReset, counters, onDelete, onIncrement } = this.props;//object destructing
-    return (
-      <div>
-        <button className="btn btn-secondary btn-sm" onClick={onReset}>
-          Reset
-        </button>
-        {counters.map(eachCounter => (
-          <Counter
-            counter={eachCounter}
-            key={eachCounter.id} //used internally by react ,cannot be accessed via props
-            onDelete={onDelete}
-            onIncrement={onIncrement}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const Counters = ({ onReset, counters, onDelete, onIncrement }) => {
+  //object destructing
+  return (
+    <div>
+      <button className="btn btn-secondary btn-sm" onClick={onReset}>
+        Reset
+      </button>
+      {counters.map(eachCounter => (
+        <Counter
+          counter={eachCounter}
+          key={eachCounter.id} //used internally by react ,cannot be accessed via props
+          onDelete={onDelete}
+          onIncrement={onIncrement}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Counters;
